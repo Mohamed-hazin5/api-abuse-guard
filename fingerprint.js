83cc9c17708e2801e75283af77582799077c225f
Mohamed-hazin5 <mohamedhazin2005@gmail.com>
2026-02-05 11:14:31 +0530

    Upgrade fingerprint with browser headers

--- fingerprint.js
+++ fingerprint.js
@@ -5,8 +5,15 @@ function generateFingerprint(req) {
         req.headers["user-agent"],
         req.headers["accept-language"],
         req.headers["accept-encoding"],
+        req.headers["sec-ch-ua"],
+        req.headers["sec-ch-ua-platform"],
+        req.headers["sec-fetch-site"],
+        req.headers["sec-fetch-mode"],
+        req.headers["sec-fetch-dest"],
         req.ip,
-    ].join("|");
+    ]
+        .filter(Boolean)
+        .join("|");
 
     return crypto.createHash("sha256").update(raw).digest("hex");
 }
